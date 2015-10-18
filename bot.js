6fa8452f6fca0e89256ab912d97337b3e885d4e0
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-10-17 22:31:55 -0700

    Use es6 const.

--- bot.js
+++ bot.js
@@ -1,17 +1,19 @@
 'use strict';
 
-var config = require('./config/config');
-var TwitterBot = require('node-twitterbot').TwitterBot;
+const config = require('./config/config');
+const TwitterBot = require('node-twitterbot').TwitterBot;
 
-var Bot = new TwitterBot({
+const bot = new TwitterBot({
   'consumer_secret': config.auth.consumerSecret,
   'consumer_key': config.auth.consumerKey,
   'access_token': config.auth.accessToken,
   'access_token_secret': config.auth.tokenSecret
 });
 
-Bot.addAction('tweet', function() {
-  Bot.tweet('H.E.L.L.O U.N.I.V.E.R.S.E');
+bot.addAction('tweet', function() {
+  bot.tweet('H.E.L.L.O U.N.I.V.E.R.S.E');
 });
 
-Bot.now('tweet');
+bot.now('tweet');
+
+console.log('something happened');
