f6d1776dc241593bf958889b1c0e9d49b664dd0c
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-10-22 22:29:41 -0700

    First galaxies spotted by the ansible.

--- src/ansible.js
+++ src/ansible.js
@@ -1,12 +1,12 @@
 'use strict';
 
-import StaticSensor from './sensor';
+import { RandomSensor } from './sensor';
 import IntervalEngine from './engine';
 import { ConsoleBroadcaster } from './broadcaster';
 
 export function beginScan() {
     let broadcaster = new ConsoleBroadcaster();
     broadcaster.broadcast('Galactic Ansible coming online...');
-    let engine = new IntervalEngine(new StaticSensor(), broadcaster, 5000);
+    let engine = new IntervalEngine(new RandomSensor(), broadcaster, 1000);
     engine.run();
 }
--- src/sensor.js
+++ src/sensor.js
@@ -1,6 +1,58 @@
 'use strict'
 
-export default class StaticSensor {
+const weighted = require('weighted');
+
+function choose(arr) {
+    return arr[Math.floor(Math.random()*arr.length)];
+}
+
+function galaxyGenerator() {
+    const galaxyName = () => {
+        const classificationName = () => {
+            let name = '';
+            do {
+                name += Math.random().toString(36).substr(0,1);
+            } while (Math.random() < 0.25);
+
+            return name;
+        };
+        const standardName = () => {
+            return choose(['Andromeda', 'Big Daddy', 'Tencent', 'Fubar', 'Johnny', 'Sixty-O', 'Dfjsktisd', 'Lemonade']);
+        }
+        
+        return choose([classificationName, standardName])()
+    };
+
+    const galaxyId = () => {
+        var name = galaxyName();
+        var type = choose(['huge', 'giant', 'colossal', 'average', 'medium', 'small', 'tiny', 'miniscule']) + ' ' + 
+            choose(['grey', 'blue', 'green', 'rainbow', 'puke-colored', 'magenta', 'black as night']) + ' ' +
+            choose(['elliptical', 'spherical', 'spiral', 'S0', 'irregular']);
+        return `The ${name} galaxy is a ${type} galaxy.`;
+    }
+
+    const galaxyCharacteristic = () => {
+        return choose(
+            ['Its nearest neighbor is ' + choose([galaxyName() + ' galaxy', 'a black hole', 'a dark matter reactor', 'the pocket universe ' + galaxyName()]),
+            'Its prominent feature is ' + choose(['a black hole', 'a dyson sphere', 'intelligent life', 'a historic event']),
+            'It has ' + Math.random() + choose([' stars', ' planets', ' fast food joints'])
+            ]);
+    }
+    return choose(
+        [
+            galaxyId,
+            galaxyCharacteristic
+        ]
+    )();
+}
+
+export class RandomSensor {
+    retrieveData() {
+        return galaxyGenerator();
+    }
+}
+
+export class StaticSensor {
     retrieveData() {
         return 'Earth is roughly a sphere.'
     }
